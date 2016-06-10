a7f093275ca7b251cdcdfea8d0ba959c39ab5dc7
Cigarent <xiaojunh1996@hotmail.com>
2016-06-10 16:20:48 -0500

    Notes add/edit function up and running

--- PlainOlNotes/app/src/main/java/com/example/plainolnotes/MainActivity.java
+++ PlainOlNotes/app/src/main/java/com/example/plainolnotes/MainActivity.java
@@ -5,23 +5,28 @@ import android.app.LoaderManager;
 import android.content.ContentValues;
 import android.content.CursorLoader;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity
+public class MainActivity extends AppCompatActivity
 implements LoaderManager.LoaderCallbacks<Cursor>
 {
+    private static final int EDITOR_REQUEST_CODE = 1001;
     private NotesCursorAdapter cursorAdapter;
 
     @Override
@@ -33,6 +38,16 @@ implements LoaderManager.LoaderCallbacks<Cursor>
 
         ListView list = (ListView) findViewById(android.R.id.list);
         list.setAdapter(cursorAdapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                Intent intent =  new Intent(MainActivity.this,EditorActivity.class);
+                Uri uri = Uri.parse(NotesProvider.CONTENT_URI+"/"+id);
+                intent.putExtra(NotesProvider.CONTENT_ITEM_TYPE, uri);
+                startActivityForResult(intent, EDITOR_REQUEST_CODE);
+            }
+        });
 
         getLoaderManager().initLoader(0, null, this);
 
@@ -122,5 +137,17 @@ implements LoaderManager.LoaderCallbacks<Cursor>
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         cursorAdapter.swapCursor(null);
+    }
+
+    public void openEditorForNewNote(View view) {
+        Intent intent = new Intent(this, EditorActivity.class);
+        startActivityForResult(intent, EDITOR_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==EDITOR_REQUEST_CODE && resultCode==RESULT_OK){
+            restartLoader();
+        }
     }
 }
--- PlainOlNotes/app/src/main/java/com/example/plainolnotes/NotesProvider.java
+++ PlainOlNotes/app/src/main/java/com/example/plainolnotes/NotesProvider.java
@@ -7,12 +7,12 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
-public class NotesProvider extends ContentProvider{
+public class NotesProvider extends ContentProvider {
 
     private static final String AUTHORITY = "com.example.plainolnotes.notesprovider";
     private static final String BASE_PATH = "notes";
     public static final Uri CONTENT_URI =
-            Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH );
+            Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH);
 
     // Constant to identify the requested operation
     private static final int NOTES = 1;
@@ -21,9 +21,11 @@ public class NotesProvider extends ContentProvider{
     private static final UriMatcher uriMatcher =
             new UriMatcher(UriMatcher.NO_MATCH);
 
+    public static final String CONTENT_ITEM_TYPE = "Note";
+
     static {
         uriMatcher.addURI(AUTHORITY, BASE_PATH, NOTES);
-        uriMatcher.addURI(AUTHORITY, BASE_PATH +  "/#", NOTES_ID);
+        uriMatcher.addURI(AUTHORITY, BASE_PATH + "/#", NOTES_ID);
     }
 
     private SQLiteDatabase database;
@@ -38,6 +40,11 @@ public class NotesProvider extends ContentProvider{
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+
+        if (uriMatcher.match(uri) == NOTES_ID) {
+            selection = DBOpenHelper.NOTE_ID + "=" + uri.getLastPathSegment();
+        }
+
         return database.query(DBOpenHelper.TABLE_NOTES, DBOpenHelper.ALL_COLUMNS,
                 selection, null, null, null,
                 DBOpenHelper.NOTE_CREATED + " DESC");
